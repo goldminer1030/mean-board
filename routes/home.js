e43d433a41dc82fbe26f274230aae68e8d528386
goldminer1030 <goldminer1030@gmail.com>
2017-10-30 05:14:47 +0100

    Project Creation

---
+++ routes/home.js
@@ -0,0 +1,14 @@
+// routes/home.js
+
+var express = require("express");
+var router = express.Router();
+
+// Home
+router.get("/", function(req, res){
+ res.render("home/welcome");
+});
+router.get("/about", function(req, res){
+ res.render("home/about");
+});
+
+module.exports = router;
